95a758274f26c166e1f97726333d29ac9d579427
Ani Siti Fatimah <153925059+Anistfatimah@users.noreply.github.com>
2025-03-14 20:22:38 +0700

    Update dashboard.py
    Mengubah code pada file

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -7,12 +7,12 @@ import seaborn as sns
 st.header('Welcome! Bike Rentals')
 
 # Load data
-day_df = pd.read_csv('day_data.csv')
-hour_df = pd.read_csv('hour_data.csv')
+d_df = pd.read_csv("day_data.csv")
+h_df = pd.read_csv("hour_data.csv")
 
 with st.sidebar:
     # Menambahkan logo perusahaan
-    st.image('sole-bicycles-unsplash.jpg')
+    st.image("sole-bicycles-unsplash.jpg")
     # Selectbox memilih musim
     season_dict = {1: "Musim Panas", 2: "Musim Semi", 3: "Musim Gugur", 4: "Musim Dingin"}
     st.sidebar.title("Filter Data")
@@ -21,7 +21,7 @@ with st.sidebar:
 
 # Filter data berdasarkan musim
 season_mapping = {v: i for i, v in season_dict.items()}
-filtered_hour_df = hour_df[hour_df["season"] == season_mapping[selected_season]]
+filtered_hour_df = h_df[h_df["season"] == season_mapping[selected_season]]
 
 # Grafik jumlah peminjaman sepeda setiap jam
 st.subheader(f"Jumlah Peminjaman Sepeda per Jam - {selected_season}")
@@ -41,7 +41,7 @@ st.markdown(
     
 
 # Grafik perbandingan paling banyak dan paling sedikit peminjaman sepeda per musim
-season_avg = day_df.groupby("season")["cnt"].mean().reset_index()
+season_avg = d_df.groupby("season")["cnt"].mean().reset_index()
 season_avg["season"] = season_avg["season"].map(season_dict)
 
 st.subheader("Perbandingan Rata-rata Peminjaman Sepeda per Musim")
